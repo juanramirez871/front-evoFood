bbb8c34533037588118e15ade9babed420cd71ec
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-21 22:12:22 -0500

    feat: :sparkles: importacion de la barra navegadora

---
+++ src/modules/introduction/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div>
+        <navegationBarVue />
+        <br><br><br><br><br>
+        
+    </div> 
+    
+</template>
+
+<script setup>
+import navegationBarVue from '../components/navegationBar.vue';
+</script>
