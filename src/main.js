213e3dff2c78483ac77a7ba01ba79ff64291b4d3
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-24 10:54:07 -0500

    feat: :sparkles: importar element ui

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './shared/router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
